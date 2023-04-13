313767cbd54b2fcef9c2ee4bbb5e87213e88b397
wilmer <est.wzelayam@uml.edu.ni>
2023-04-13 12:10:12 -0600

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content:["./src/**/*.{html,js}","*.html","*.css"],
-  theme: {
+  theme: ["light"],
     extend: {},
-  },
+  
   plugins: [],
 }
 
